a184a3086eb8305003015acaa27d4cf97efa4cd2
palashco-lav <palashco@gmail.com>
2025-09-18 10:08:34 +0300

    Revert "Выполнение ДЗ 2"

--- library/urls.py
+++
@@ -1,7 +0,0 @@
-# library/urls.py
-from django.urls import path
-from . import views  # Импортируем views из текущего приложения
-
-urlpatterns = [
-    path('', views.index, name='index'), # Можно раскомментировать эту строку
-]
--- library/views.py
+++
@@ -1,8 +0,0 @@
-# library/views.py
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Простейшая view-функция для примера.
-# Её наличие позволяет успешно импортировать модуль 'views'.
-def index(request):
-    return HttpResponse("Hello from library app!")
