7ac2e544c401cde10521832ec7823772857ff5cd
palashco-lav <palashco@gmail.com>
2025-10-15 21:33:43 +0300

    Выполнение домашки 28 Права доступа

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 # products/views.py
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import HttpResponseForbidden
 from .models import Product
 
 
@@ -18,7 +20,6 @@ class ProductsListView(ListView):
     context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
-        """Добавляем заголовок в контекст"""
         context = super().get_context_data(**kwargs)
         context['title'] = 'Каталог товаров'
         return context
@@ -30,23 +31,25 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'product_id'
-    login_url = '/users/login/'  # URL для перенаправления неавторизованных пользователей
+    login_url = '/users/login/'
 
     def get_context_data(self, **kwargs):
-        """Добавляем заголовок в контекст"""
         context = super().get_context_data(**kwargs)
         context['title'] = f'{self.object.name} - Детальная информация'
         return context
 
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
+    """Создание продукта - автоматически привязываем к текущему пользователю"""
     model = Product
-    template_name = 'products/product_form.html'  # Убедитесь, что путь правильный
-    fields = ['name', 'description', 'price', 'category', 'in_stock', 'image']
+    template_name = 'products/product_form.html'
+    fields = ['name', 'description', 'price', 'category', 'in_stock', 'image', 'publication_status']
     success_url = reverse_lazy('products:list')
 
     def form_valid(self, form):
+        """Автоматически привязываем продукт к текущему пользователю"""
         form.instance.owner = self.request.user
+        messages.success(self.request, f'Продукт "{form.instance.name}" успешно создан!')
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
@@ -55,12 +58,43 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
         return context
 
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
+class OwnerRequiredMixin(UserPassesTestMixin):
+    """Миксин для проверки, что пользователь является владельцем продукта"""
+
+    def test_func(self):
+        product = self.get_object()
+        return product.owner == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'У вас нет прав для редактирования этого продукта.')
+        return HttpResponseForbidden('Доступ запрещен')
+
+
+class ModeratorOrOwnerRequiredMixin(UserPassesTestMixin):
+    """Миксин для проверки, что пользователь является владельцем ИЛИ модератором"""
+
+    def test_func(self):
+        product = self.get_object()
+        user = self.request.user
+        # Владелец ИЛИ пользователь с правом удаления продуктов (модератор)
+        return product.owner == user or user.has_perm('products.delete_product')
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'У вас нет прав для удаления этого продукта.')
+        return HttpResponseForbidden('Доступ запрещен')
+
+
+class ProductUpdateView(LoginRequiredMixin, OwnerRequiredMixin, UpdateView):
+    """Только владелец может редактировать"""
     model = Product
-    template_name = 'products/product_form.html'  # Убедитесь, что путь правильный
-    fields = ['name', 'description', 'price', 'category', 'in_stock', 'image']
+    template_name = 'products/product_form.html'
+    fields = ['name', 'description', 'price', 'category', 'in_stock', 'image', 'publication_status']
     pk_url_kwarg = 'product_id'
     success_url = reverse_lazy('products:list')
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Продукт "{form.instance.name}" успешно обновлен!')
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -68,16 +102,46 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         return context
 
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+class ProductDeleteView(LoginRequiredMixin, ModeratorOrOwnerRequiredMixin, DeleteView):
+    """Владелец ИЛИ модератор может удалять"""
     model = Product
-    template_name = 'products/product_confirm_delete.html'  # Убедитесь, что путь правильный
+    template_name = 'products/product_confirm_delete.html'
     pk_url_kwarg = 'product_id'
     success_url = reverse_lazy('products:list')
+
+    def delete(self, request, *args, **kwargs):
+        product = self.get_object()
+        messages.success(request, f'Продукт "{product.name}" успешно удален!')
+        return super().delete(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Удалить {self.object.name}'
         return context
+
+
+# Дополнительный view для снятия с публикации
+from django.views import View
+from django.shortcuts import redirect
+
+
+class ProductUnpublishView(LoginRequiredMixin, View):
+    """Снятие продукта с публикации - только модераторы"""
+
+    def post(self, request, product_id):
+        if not request.user.has_perm('products.can_unpublish_product'):
+            messages.error(request, 'У вас нет прав для снятия продуктов с публикации.')
+            return redirect('products:list')
+
+        product = get_object_or_404(Product, id=product_id)
+        if product.publication_status == 'published':
+            product.publication_status = 'unpublished'
+            product.save()
+            messages.success(request, f'Продукт "{product.name}" снят с публикации!')
+        else:
+            messages.warning(request, 'Продукт уже не опубликован')
+
+        return redirect('products:list')
 
 
 class TestTemplateView(TemplateView):
